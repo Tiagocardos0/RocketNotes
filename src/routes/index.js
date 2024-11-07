941917876eb3a29fe9de1df292f0cf2bf8fdc2ea
Tiago Cardoso <tiago.card2121@gmail.com>
2024-11-07 20:23:20 -0300

    Criado controller de tags e rotas, para listar as tags ou exibi-las e finalizado o projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
 const MovieNotesRouter = require('./MovieNotes.routes');
-
+const tagsRouter = require('./tags.routes');
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/movienotes', MovieNotesRouter);
+routes.use('/tags', tagsRouter);
 module.exports = routes;
